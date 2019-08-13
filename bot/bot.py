ed7e6411f676d64db463c26a690fb16152ce9aa5
ludeeus <joasoe@gmail.com>
2019-08-13 22:41:30 +0200

    Fix submitter issue

--- bot/bot.py
+++ bot/bot.py
@@ -39,18 +39,19 @@ class Bot:
             self.event_data["repository"]["full_name"]
         )
 
+        self.submitter = self.event_data["sender"]["login"]
+        self.action = self.event_data["action"]
+
         if self.event_data.get("pull_request") is not None:
             print("We now know that this is a PR.")
             self.issue_type = "pr"
-            self.issue_number = self.event_data["number"]
+            self.submitter = self.event_data["pull_request"]["user"]["login"]
+            self.issue_number = self.event_data["pull_request"]["number"]
 
         elif self.event_data.get("issue") is not None:
             print("We now know that this is a issue.")
             self.issue_type = "issue"
             self.issue_number = self.event_data["issue"]["number"]
-
-        self.submitter = self.event_data["sender"]["login"]
-        self.action = self.event_data["action"]
 
         self.issue_comment = IssueComment(self.issue_number, self.repository)
         self.issue_update = IssueUpdate(self.issue_number, self.repository)
