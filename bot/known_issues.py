1dec050b19dd031829382b63b56905d0916e373f
ludeeus <joasoe@gmail.com>
2019-08-14 16:47:28 +0200

    Fix known issue handler

--- bot/known_issues.py
+++ bot/known_issues.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-docstring,line-too-long,broad-except
 KNOWN_ISSUES = [
     {
-        "check": ["[hacs] Missing migration step for 4"],
+        "check": ["Missing migration step for 4"],
         "description": "HACS version 0.13.0+ can **only** migrate from version 0.12.0+.\n\nTo fix this issue you need to first upgrade to version 0.12.0, then upgrade to 0.13.0\n\nOr you can delete all the files containing `hacs` in the `.storage` directory and restart Home Assistant **(NB!: This will delete the status of the repositories you have installed.)**",
     }
 ]
